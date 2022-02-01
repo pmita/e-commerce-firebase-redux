e57b594ba24495eb4edda5e056b77b640ab4bedf
pmita <panos.mita2@outlook.com>
2022-02-01 20:57:50 +0000

    adding all the routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from 'react'
 import Navbar from './components/Navbar';
 //ROUTER
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Banner from './components/Banner';
+//PAGES
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Cart from './pages/Cart/Cart';
+import Contact from './pages/Contact/Contact';
+import Product from './pages/Product/Product';
+import Products from './pages/Products/Products';
+import Signin from './pages/Signin/Signin';
+import Signout from './pages/Signout/Signout';
 
 function App() {
   return (
@@ -11,7 +19,14 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Banner />} />
+          <Route path='/' exact element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/about' element={<Cart />} />
+          <Route path='/about' element={<Contact />} />
+          <Route path='/about' exact element={<Product />} />
+          <Route path='/about' element={<Products />} /> 
+          <Route path='/about' element={<Signin />} /> 
+          <Route path='/about' element={<Signout />} />
         </Routes>
       </div>
     </Router>
